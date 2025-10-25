f758f8b2a9b0247f98d27639ad6e418fe8d9ff7d
skidaim <66956355+skidaim@users.noreply.github.com>
2025-10-25 15:30:21 +0300

    Update main.c

--- init/main.c
+++ init/main.c
@@ -1093,7 +1093,7 @@ void start_kernel(void)
 
 	acpi_subsystem_init();
 	arch_post_acpi_subsys_init();
-	kcsan_init()
+	kcsan_init();
 
 	/* Do the rest non-__init'ed, we're now alive */
 	rest_init();
